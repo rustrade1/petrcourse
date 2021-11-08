67495e04b2e0dd0fd44cf38d47c12b73ec05f117
Ruslan <rusakdemon@gmail.com>
2021-11-08 21:06:47 +0500

    lesson29Important

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('. heart'),
+      oneHeart = document.querySelector('.heart');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+btns[1].style.borderRadius = '100%';    
+
+box.style.cssText = 'background-color: blue; width: 500px';

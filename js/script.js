49efa9d1d90767667e5e747502400bdeff00b56c
Ruslan <rusakdemon@gmail.com>
2021-11-26 00:25:46 +0500

    importantTIMER

--- js/script.js
+++ js/script.js
@@ -1,125 +1,89 @@
-// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+window.addEventListener('DOMContentLoaded', () => {
 
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-// let addFilm = function (e) {
-//     e.preventDefault();
-//     if (inp.value.length > 21) {
-//         let slice = inp.value.slice(0, 21);
-//         movieDB.movies.push(slice + '...');
-//     } else {
-//         movieDB.movies.push(inp.value);
-//     }
-//     if (checkbo.checked) {
-//         console.log("Добавляем любимый фильм");
-//     }
-//     movieDB.movies.sort();
-//     arr.innerHTML = '';
-//     movieDB.movies.forEach((film, i) => {
-//         arr.innerHTML +=
-//             `<li class="promo__interactive-item">${i + 1} ${film}
-//                 <div class="delete"></div>
-//             </li>`;
-//     });
-// };
-// btn.addEventListener('click', addFilm);
-
-
-
-// for (let i = 0; i < del.length; i++) {
-//     del[i].addEventListener('click', (e) => {
-//         e.preventDefault();
-//         delete movieDB.movies[i];
-//         console.log('удалено');
-//         arr.innerHTML = '';
-//         movieDB.movies.forEach((film, k) => {
-//             arr.innerHTML +=
-//                 `<li class="promo__interactive-item">${k + 1} ${film}
-//                 <div class="delete"></div>
-//             </li>`;
-//         });
-//     });
-// }
-
-
-// 5) Добавить нумерацию выведенных фильмов */
-
-// 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-// новый фильм добавляется в список. Страница не должна перезагружаться.
-// Новый фильм должен добавляться в movieDB.movies.
-// Для получения доступа к значению input - обращаемся к нему как input.value;
-// P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-// 2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-// 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-let imgArr = document.querySelectorAll(".promo__adv img");
-
-
-const deleteAdv = (arr) => {
-    arr.forEach(item => {
-        item.remove();
-    });
-};
-
-document.querySelector('.promo__genre').textContent = 'Драма';
-document.querySelector(".promo__bg").style.backgroundImage = 'url(img/bg.jpg)';
-
-let arr = document.querySelector(".promo__interactive-list");
-let btn = document.querySelector('button');
-let inp = document.querySelector('.adding__input');
-let checkbo = document.querySelector('[type="checkbox"]');
-let del = document.querySelectorAll('.delete');
-let addForm = document.querySelector('form.add');
-
-const sortArr = function (arr) {
-    arr.sort();
-};
-
-addForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let newFilm = inp.value;
-    const favorite = checkbo.checked;  
-    if (newFilm) {
-        if (newFilm.length > 21) {
-            newFilm = `${newFilm.substring(0, 22)}...`
-        }
-        if (favorite) {
-            console.log('заебумба');
-        }
-        movieDB.movies.push(newFilm);
-        sortArr(movieDB.movies);
-        createMoveList(movieDB.movies, arr);
-    }
-    event.target.reset();
-});
-
-function createMoveList(films, parent) {
-    parent.innerHTML = '';
-    sortArr(films);
-    films.forEach((film, i) => {
-        parent.innerHTML +=
-            `<li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>`;
-    });
-    document.querySelectorAll('.delete').forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            btn.parentElement.remove();
-            movieDB.movies.splice(i, 1);
-            createMoveList(films, parent);
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
         });
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+
+    }
+    hideTabContent();
+    showTabContent();
+
+
+    tabsParent.addEventListener('click', (e) => {
+        const targer = e.target;
+        if (targer && targer.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (targer == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
     });
-}
-createMoveList(movieDB.movies, arr);
-deleteAdv(imgArr);
-// sortArr(movieDB.movies);
+
+
+
+    // Timer
+    const deadline = '2021-11-30';
+
+    function getTimeRemaning(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+    }
+    setClock('.timer', deadline);
+
+});
